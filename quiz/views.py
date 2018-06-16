9bd38c04b3f828df1521ce7b2c4f6377b8d51624
Ploy <min-ploy@windowslive.com>
2018-06-16 22:57:35 +0700

    Can show count votes

--- quiz/views.py
+++ quiz/views.py
@@ -8,4 +8,13 @@ def homepage(request):
     return render(request, 'homepage.html', {'ques_all': ques_all})
 
 def vote(request):
-    return render(request, 'vote.html')
+    question = Question.objects.get(pk=2)
+    try:
+        selected_answer = question.ans_set.get(pk=request.POST['answer'])
+    except (KeyError, Ans.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'vote.html', {'question': question})
+    else:
+        selected_answer.votes += 1
+        selected_answer.save()
+    return render(request, 'vote.html', {'question': question})
